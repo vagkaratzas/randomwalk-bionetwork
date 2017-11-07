f9ea32575b1fee7bd2552708754074b73e879096
Evangelos Karatzas <32259775+vagkaratzas@users.noreply.github.com>
2017-11-07 11:35:36 +0200

    Update randomwalk-bionetwork.R

--- randomwalk-bionetwork.R
+++ randomwalk-bionetwork.R
@@ -1,3 +1,4 @@
+#!/usr/bin/env Rscript
 # randomwalk-bionetwork
 #Starting from a specific disease, the user is allowed to command random walkers to explore a multi level network of their own input choosing and are returned an edgelist with the walked edges and their aggregate step count values.
 
